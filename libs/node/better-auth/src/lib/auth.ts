d840a7e73b4f02e59a185ca79c96560305e4e69c
balajidharma <balajidharma@hotmail.com>
2026-01-25 20:49:25 -0500

    Added postgres db

--- libs/node/better-auth/src/lib/auth.ts
+++ libs/node/better-auth/src/lib/auth.ts
@@ -1,12 +1,13 @@
 import { betterAuth } from "better-auth";
 import { username } from "better-auth/plugins"
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from '@prisma/client/mongodb-app/client.js';
+import { Pool } from "pg";
 
-const prisma = new PrismaClient();
 export const auth = betterAuth({
     advanced: {
         disableOriginCheck: process.env['NODE_ENV'] !== 'production',
+        database: {
+            generateId: 'uuid'
+        }
     },
     emailAndPassword: {
         enabled: true,
@@ -21,8 +22,55 @@ export const auth = betterAuth({
 
         },
     },
-    database: prismaAdapter(prisma, {
-        provider: "mongodb", // or "mysql", "postgresql", ...etc
+    database: new Pool({
+        host: process.env['DB_HOST'] || 'localhost',
+        port: Number(process.env['DB_PORT'] || 5432),
+        user: process.env['DB_USERNAME'] || 'boat',
+        password: process.env['DB_PASSWORD'],
+        database: process.env['DB_DATABASE'],
     }),
+    user: {
+        modelName: "users",
+        fields: {
+            emailVerified: "email_verified",
+            createdAt: "created_at",
+            updatedAt: "updated_at",
+            displayUsername: "display_username"
+        },
+    },
+    account: {
+        modelName: "accounts",
+        fields: {
+            accountId: "account_id",
+            providerId: "provider_id",
+            userId: "user_id",
+            accessToken: "access_token",
+            refreshToken: "refresh_token",
+            idToken: "id_token",
+            accessTokenExpiresAt: "access_token_expires_at",
+            refreshTokenExpiresAt: "refresh_token_expires_at",
+            createdAt: "created_at",
+            updatedAt: "updated_at",
+        },
+    },
+    session: {
+        modelName: "sessions",
+        fields: {
+            expiresAt: "expires_at",
+            createdAt: "created_at",
+            updatedAt: "updated_at",
+            ipAddress: "ip_address",
+            userAgent: "user_agent",
+            userId: "user_id"
+        },
+    },
+    verification: {
+        modelName: "verifications",
+        fields: {
+            expiresAt: "expires_at",
+            createdAt: "created_at",
+            updatedAt: "updated_at",
+        },
+    },
     plugins: [username()],
 });
